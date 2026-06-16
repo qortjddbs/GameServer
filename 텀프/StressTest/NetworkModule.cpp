182f9d0fc4da4babf9ccd705763cb976f9f7ce34
qortjddbs <117032925+qortjddbs@users.noreply.github.com>
2026-06-16 10:31:33 +0900

    DB 백업

--- 텀프/StressTest/NetworkModule.cpp
+++ 텀프/StressTest/NetworkModule.cpp
@@ -22,7 +22,7 @@ using namespace chrono;
 
 extern HWND		hWnd;
 
-const static int MAX_TEST = 5000;
+const static int MAX_TEST = 10000;
 const static int MAX_CLIENTS = MAX_TEST * 2;
 const static int INVALID_ID = -1;
 const static int MAX_PACKET_SIZE = 255;
